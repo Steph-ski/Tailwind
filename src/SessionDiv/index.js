89b4e68eedc88433a47b845fe5fd7ade5c631c5d
Steph-ski <stephaniejohns57@gmail.com>
2023-04-18 16:53:46 +0100

    session div component added

---
+++ src/SessionDiv/index.js
@@ -0,0 +1,9 @@
+const SessionDiv = () => {
+    return (
+            <div class="text-sm mt-6 mx-6 p-6 bg-amber-400 border-2 border-current rounded-lg flex flex-wrap text-center space-x-1 justify-center">
+                <p>Tip: You can find bookmarked session in your </p>
+                <p class="underline underline-offset-1"> Developer Profile.</p>
+            </div>
+    )
+}
+export default SessionDiv
